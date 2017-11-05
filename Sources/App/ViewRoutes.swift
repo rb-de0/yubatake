9e7ba933f9d8ba0a13d98a592f438c3da7b0ad5d
rb_de0 <rebirth.de0@gmail.com>
2017-11-05 21:32:47 +0900

    Implement admin views

--- Sources/App/ViewRoutes.swift
+++ Sources/App/ViewRoutes.swift
@@ -11,7 +11,11 @@ final class ViewRoutes: RouteCollection {
     
     func build(_ builder: RouteBuilder) throws {
         
+        // Admin
         builder.get("admin/login") { _ in try self.view.make("admin/login") }
         builder.get("admin/posts") { _ in try self.view.make("admin/posts") }
+        builder.get("admin/tags") { _ in try self.view.make("admin/tags") }
+        builder.get("admin/categories") { _ in try self.view.make("admin/categories") }
+        builder.get("admin/new-post") { _ in try self.view.make("admin/new-post") }
     }
 }
