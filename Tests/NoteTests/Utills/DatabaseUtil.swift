08b1ecc6fb04e925624ec3182138ccc1b58c3c3d
rb_de0 <rebirth.de0@gmail.com>
2018-02-19 20:55:00 +0900

    Add swift test

--- Tests/NoteTests/Utills/DatabaseUtil.swift
+++ Tests/NoteTests/Utills/DatabaseUtil.swift
@@ -1,10 +1,27 @@
+import Foundation
 import MySQL
 import MySQLDriver
 
 final class DB {
     
+    class var hostName: String {
+        return ProcessInfo().environment["MYSQL_HOSTNAME"] ?? "127.0.0.1"
+    }
+    
+    class var user: String {
+        return ProcessInfo().environment["MYSQL_USER"] ?? "root"
+    }
+    
+    class var password: String {
+        return ProcessInfo().environment["MYSQL_PASSWORD"] ?? "root"
+    }
+    
+    class var port: UInt {
+        return ProcessInfo().environment["MYSQL_PORT"]?.uint ?? 3307
+    }
+    
     private class func driver() throws -> MySQLDriver.Driver {
-        let dataBase = try MySQL.Database(hostname: "127.0.0.1", user: "root", password: "root", database: "", port: 3307)
+        let dataBase = try MySQL.Database(hostname: hostName, user: user, password: password, database: "", port: port)
         return MySQLDriver.Driver(master: dataBase)
     }
     
