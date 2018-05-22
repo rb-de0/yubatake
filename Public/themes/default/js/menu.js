5ef3ccf01a12aa83695a18b93964583cb1eece05
rb_de0 <rebirth.de0@gmail.com>
2018-05-22 16:23:57 +0900

    Create default theme

--- Public/themes/default/js/menu.js
+++
@@ -1,9 +0,0 @@
-var menuViewModel = new Vue({
-  el: '#toggle-menu',
-  methods: {
-    toggleMenu: function (e) {
-      document.getElementById('menu').classList.toggle('open-menu')
-      document.getElementById('toggle-menu').classList.toggle('open-menu')
-    }
-  }
-})
