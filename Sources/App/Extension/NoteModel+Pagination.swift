7fb6628a08555902bb4aa5aad122a2a2263c9127
rb_de0 <rebirth.de0@gmail.com>
2018-05-25 12:40:56 +0900

    Update pagination logic to method using library

--- Sources/App/Extension/NoteModel+Pagination.swift
+++ Sources/App/Extension/NoteModel+Pagination.swift
@@ -4,35 +4,12 @@ import Vapor
 
 extension QueryBuilder where Model: DatabaseModel, Model: Paginatable {
     
-    func paginate(for request: Request, key: String = Pagination.defaultPageKey) throws -> Future<Page<Model>> {
-        
+    func paginate(for request: Request) throws -> Future<Page<Model>> {
+        let key = Pagination.defaultPageKey
+        let field: KeyPath<Model, Date?> = \Model.createdAt
+        let sorts = [try QuerySort(field: field.makeQueryField(), direction: .descending)]
         let page = try request.query.get(Int?.self, at: key) ?? 1
-        let pageSize = Model.defaultPageSize
-        let lower = pageSize * (page - 1)
-        let upper = page * pageSize
-        
-        let countBuilder = Model.query(on: request)
-        countBuilder.query = query
-        
-        let resultsBuilder = Model.query(on: request)
-        resultsBuilder.query = query
-        
-        let totalCount = countBuilder.count()
-        let results = try resultsBuilder
-            .sort(\Model.createdAt, .descending)
-            .range(lower: lower, upper: upper)
-            .all()
-        
-        return totalCount.and(results)
-            .map { data in
-                let (total, results) = data
-                return try Page<Model>(
-                    number: page,
-                    data: results,
-                    size: pageSize,
-                    total: total
-                )
-        }
+        return try paginate(page: page, sorts)
     }
 }
 
