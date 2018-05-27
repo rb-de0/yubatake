f199c61339b8d620d2de45aa90c0c645a3ce7c0a
rb_de0 <rebirth.de0@gmail.com>
2018-05-27 11:42:41 +0900

    Fix meta encoding logic

--- Sources/App/Config/Configs/ApplicationConfig.swift
+++ Sources/App/Config/Configs/ApplicationConfig.swift
@@ -13,13 +13,39 @@ struct ApplicationConfig: LocalConfig, Service {
     let meta: Meta?
     
     struct Meta: Content {
+
+        private enum EncodingKeys: String, CodingKey {
+            case pageDescription = "page_description"
+            case pageImage = "page_image"
+            case twitter
+        }
+        
         let pageDescription: String
         let pageImage: String
         let twitter: TwitterMeta?
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: EncodingKeys.self)
+            try container.encode(pageDescription, forKey: .pageDescription)
+            try container.encode(pageImage, forKey: .pageImage)
+            try container.encodeIfPresent(twitter, forKey: .twitter)
+        }
     }
     
     struct TwitterMeta: Content {
+
+        private enum EncodingKeys: String, CodingKey {
+            case imageAlt = "image_alt"
+            case username
+        }
+        
         let imageAlt: String
         let username: String
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: EncodingKeys.self)
+            try container.encode(imageAlt, forKey: .imageAlt)
+            try container.encode(username, forKey: .username)
+        }
     }
 }
